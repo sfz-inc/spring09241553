cad2e251ade677597b0adb4dac5e87a9eb11fb29
Ryan Day <jrday@users.noreply.github.com>
2018-09-24 15:53:29 -0400

    Initial commit from Atomist

---
+++ src/main/java/gl/Spring09241553Application.java
@@ -0,0 +1,12 @@
+package gl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09241553Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring09241553Application.class, args);
+	}
+}
